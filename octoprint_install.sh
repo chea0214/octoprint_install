9d6d786bda5d52371cadc0f3c8587b748f7b7e14
chea0214 <74455941+chea0214@users.noreply.github.com>
2023-10-14 21:37:59 +0800

    Update octoprint_install.sh

--- octoprint_install.sh
+++ octoprint_install.sh
@@ -6,19 +6,6 @@ if (($EUID != 0)); then
     exit
 fi
 
-#this is a weak check, but will catch most cases
-if [ $SUDO_USER ]; then
-    user=$SUDO_USER
-else
-    echo "You should not run this script as root. Use sudo as a normal user"
-    exit
-fi
-
-if [ "$user" == root ]; then
-    echo "You should not run this script as root. Use sudo as a normal user"
-    exit
-fi
-
 SCRIPTDIR=$(dirname $(readlink -f $0))
 
 # from stackoverflow.com/questions/3231804
